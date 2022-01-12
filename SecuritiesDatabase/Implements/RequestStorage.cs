e6bde8319bc0fa078b6d264302c44a2cd011e32b
VladArzamasov <71029515+VladArzamasov@users.noreply.github.com>
2022-01-12 17:37:27 +0400

    изменил

--- SecuritiesDatabase/Implements/RequestStorage.cs
+++ SecuritiesDatabase/Implements/RequestStorage.cs
@@ -15,13 +15,19 @@ namespace SecuritiesDatabase.Implements
         {
             using (var context = new securitiesdbContext())
             {
-                return context.Request.Select(rec => new RequestViewModel
+                return context.Request
+                    .Include(rec => rec.Bag)
+                    .ThenInclude(rec => rec.Security)
+                    .ToList().Select(rec => new RequestViewModel
                 {
                     Id = rec.Id,
                     RequestDate = (DateTime)rec.Date,
                     RequestSum = (decimal)rec.Sum,
                     ClientName = rec.Client.Fio,
-                    AgentName = rec.Agent.Fio
+                    AgentName = rec.Agent.Fio,
+                    RequestSecurity = rec.Bag
+                    .ToDictionary(recD => recD.SecurityId,
+                    recD => (recD.Security?.SecurityName, recD.Security?.SalePrice))
                 }).ToList();
             }
         }
@@ -34,14 +40,19 @@ namespace SecuritiesDatabase.Implements
             }
             using (var context = new securitiesdbContext())
             {
-                return context.Request.Where(rec => rec.Id.Equals(model.Id)).Select(rec => new RequestViewModel
-                {
-                    Id = rec.Id,
-                    RequestDate = (DateTime)rec.Date,
-                    RequestSum = (decimal)rec.Sum,
-                    ClientName = rec.Client.Fio,
-                    AgentName = rec.Agent.Fio
-                }).ToList();
+                return context.Request
+                    .Include(rec => rec.Bag)
+                    .ThenInclude(rec => rec.Security)
+                    .Where(rec => rec.Id.Equals(model.Id)).ToList()
+                    .Select(rec => new RequestViewModel
+                    {
+                        Id = rec.Id,
+                        RequestDate = (DateTime)rec.Date,
+                        RequestSum = (decimal)rec.Sum,
+                        ClientName = rec.Client.Fio,
+                        AgentName = rec.Agent.Fio,
+                        RequestSecurity = rec.Bag.ToDictionary(recD => recD.SecurityId, recD => (recD.Security?.SecurityName, recD.Security?.SalePrice))
+                    }).ToList();
             }
         }
 
@@ -54,7 +65,10 @@ namespace SecuritiesDatabase.Implements
 
             using (var context = new securitiesdbContext())
             {
-                var request = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
+                var request = context.Request
+                    .Include(rec => rec.Bag)
+                    .ThenInclude(rec => rec.Security)
+                    .FirstOrDefault(rec => rec.Id == model.Id);
                 return request != null ?
                 new RequestViewModel
                 {
@@ -62,7 +76,8 @@ namespace SecuritiesDatabase.Implements
                     RequestDate = (DateTime)request.Date,
                     RequestSum = (decimal)request.Sum,
                     ClientName = request.Client.Fio,
-                    AgentName = request.Agent.Fio
+                    AgentName = request.Agent.Fio,
+                    RequestSecurity = request.Bag.ToDictionary(recD => recD.SecurityId, recD => (recD.Security?.SecurityName, recD.Security?.SalePrice))
                 } : null;
             }
         }
@@ -75,7 +90,7 @@ namespace SecuritiesDatabase.Implements
                 {
                     try
                     {
-                        context.Request.Add(CreateModel(model, new Request(), context));
+                        CreateModel(model, new Request(), context);
                         context.SaveChanges();
                         transaction.Commit();
                     }
@@ -97,17 +112,21 @@ namespace SecuritiesDatabase.Implements
                     try
                     {
                         var element = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
+
                         if (element == null)
                         {
                             throw new Exception("Элемент не найден");
                         }
+
                         CreateModel(model, element, context);
                         context.SaveChanges();
+
                         transaction.Commit();
                     }
                     catch
                     {
                         transaction.Rollback();
+
                         throw;
                     }
                 }
@@ -137,6 +156,47 @@ namespace SecuritiesDatabase.Implements
             request.Sum = model.RequestSum;
             request.ClientId = model.ClientId;
             request.AgentId = model.AgentId;
+
+            if (request.Id == 0)
+            {
+                context.Request.Add(request);
+                context.SaveChanges();
+            }
+
+            if (model.Id.HasValue)
+            {
+                var Bag = context.Bag
+                    .Where(rec => rec.RequestId == model.Id.Value)
+                    .ToList();
+
+                context.Bag
+                    .RemoveRange(Bag
+                        .Where(rec => !model.RequestSecurity
+                            .ContainsKey((int)rec.SecurityId))
+                                .ToList());
+                context.SaveChanges();
+
+                foreach (var updateDetail in Bag)
+                {
+                    updateDetail.Sum = model.RequestSecurity[(int)updateDetail.SecurityId].Item2;
+                    model.RequestSecurity.Remove((int)updateDetail.SecurityId);
+                }
+
+                context.SaveChanges();
+            }
+
+            foreach (var ed in model.RequestSecurity)
+            {
+                context.Bag.Add(new Bag
+                {
+                    RequestId = request.Id,
+                    SecurityId = ed.Key,
+                    Sum = ed.Value.Item2
+                });
+
+                context.SaveChanges();
+            }
+
             return request;
         }
     }
