dae49c82ccbe1412a00f1c5b00bc1921cef02ceb
VladArzamasov <71029515+VladArzamasov@users.noreply.github.com>
2022-01-12 23:21:36 +0400

    Сделал Storage

---
+++ SecuritiesDatabase/Implements/BagStorage.cs
@@ -0,0 +1,194 @@
+﻿using SecuritiesBusinessLogic.BindingModels;
+using SecuritiesBusinessLogic.Interfaces;
+using SecuritiesBusinessLogic.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecuritiesDatabase.Implements
+{
+    public class BagStorage : IBagStorage
+    {
+        public List<BagViewModel> GetFullList()
+        {
+            using (var context = new securitiesdbContext())
+            {
+                return context.Bag
+                    .Include(rec => rec.BagSecurities)
+                    .ThenInclude(rec => rec.Securities)
+                    .ToList().Select(rec => new BagViewModel
+                    {
+                        Id = rec.Id,
+                        Status = rec.Status,
+                        Sum = (decimal)rec.Sum,
+                        BagSecurities = rec.BagSecurities
+                    .ToDictionary(recD => recD.SecuritiesId,
+                    recD => (recD.Securities?.Name, recD.Sum))
+                    }).ToList();
+            }
+        }
+
+        public List<BagViewModel> GetFilteredList(BagBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new securitiesdbContext())
+            {
+                return context.Bag
+                    .Include(rec => rec.BagSecurities)
+                    .ThenInclude(rec => rec.Securities)
+                    .Where(rec => rec.Id.Equals(model.Id)).ToList()
+                    .Select(rec => new BagViewModel
+                    {
+                        Id = rec.Id,
+                        Status = rec.Status,
+                        Sum = (decimal)rec.Sum,
+                        BagSecurities = rec.BagSecurities.ToDictionary(recD => recD.SecuritiesId, recD => (recD.Securities?.Name, recD.Sum))
+                    }).ToList();
+            }
+        }
+
+        public BagViewModel GetElement(BagBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+
+            using (var context = new securitiesdbContext())
+            {
+                var bag = context.Bag
+                    .Include(rec => rec.BagSecurities)
+                    .ThenInclude(rec => rec.Securities)
+                    .FirstOrDefault(rec => rec.Id == model.Id);
+                return bag != null ?
+                new BagViewModel
+                {
+                    Id = bag.Id,
+                    Status = bag.Status,
+                    Sum = (decimal)bag.Sum,
+                    BagSecurities = bag.BagSecurities.ToDictionary(recD => recD.SecuritiesId, recD => (recD.Securities?.Name, recD.Sum))
+                } : null;
+            }
+        }
+
+        public void Insert(BagBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        CreateModel(model, new Bag(), context);
+                        context.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public void Update(BagBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var element = context.Bag.FirstOrDefault(rec => rec.Id == model.Id);
+
+                        if (element == null)
+                        {
+                            throw new Exception("Элемент не найден");
+                        }
+
+                        CreateModel(model, element, context);
+                        context.SaveChanges();
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+
+                        throw;
+                    }
+                }
+            }
+        }
+
+        public void Delete(BagBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                Bag element = context.Bag.FirstOrDefault(rec => rec.Id == model.Id);
+                if (element != null)
+                {
+                    context.Bag.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Элемент не найден");
+                }
+            }
+        }
+
+        private Bag CreateModel(BagBindingModel model, Bag bag, securitiesdbContext context)
+        {
+            bag.Status = model.Status;
+            bag.Sum = model.Sum;
+
+            if (bag.Id == 0)
+            {
+                context.Bag.Add(bag);
+                context.SaveChanges();
+            }
+
+            if (model.Id.HasValue)
+            {
+                var BagSecurities = context.BagSecurities
+                    .Where(rec => rec.BagId == model.Id.Value)
+                    .ToList();
+
+                context.BagSecurities
+                    .RemoveRange(BagSecurities
+                        .Where(rec => !model.BagSecurities
+                            .ContainsKey((int)rec.SecuritiesId))
+                                .ToList());
+                context.SaveChanges();
+
+                foreach (var updateDetail in BagSecurities)
+                {
+                    updateDetail.Sum = model.BagSecurities[(int)updateDetail.SecuritiesId].Item2;
+                    model.BagSecurities.Remove((int)updateDetail.SecuritiesId);
+                }
+
+                context.SaveChanges();
+            }
+
+            foreach (var ed in model.BagSecurities)
+            {
+                context.BagSecurities.Add(new BagSecurities
+                {
+                    BagId = bag.Id,
+                    SecuritiesId = ed.Key,
+                    Sum = ed.Value.Item2
+                });
+
+                context.SaveChanges();
+            }
+
+            return bag;
+        }
+    }
+}
--- SecuritiesDatabase/Implements/RequestStorage.cs
+++ SecuritiesDatabase/Implements/RequestStorage.cs
@@ -1,204 +1,164 @@
-﻿//using SecuritiesBusinessLogic.BindingModels;
-//using SecuritiesBusinessLogic.Interfaces;
-//using SecuritiesBusinessLogic.ViewModels;
-//using SecuritiesDatabase.Models;
-//using Microsoft.EntityFrameworkCore;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using SecuritiesBusinessLogic.BindingModels;
+using SecuritiesBusinessLogic.Interfaces;
+using SecuritiesBusinessLogic.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace SecuritiesDatabase.Implements
-//{
-//    public class RequestStorage : IRequestStorage
-//    {
-//        public List<RequestViewModel> GetFullList()
-//        {
-//            using (var context = new securitiesdbContext())
-//            {
-//                return context.Request
-//                    .Include(rec => rec.Bag)
-//                    .ThenInclude(rec => rec.Security)
-//                    .ThenInclude(rec => rec.Emitent)
-//                    .ToList().Select(rec => new RequestViewModel
-//                {
-//                    Id = rec.Id,
-//                    RequestDate = (DateTime)rec.Date,
-//                    RequestSum = (decimal)rec.Sum,
-//                    ClientName = rec.Client.Fio,
-//                    AgentName = rec.Agent.Fio,
-//                    RequestSecurity = rec.Bag
-//                    .ToDictionary(recD => recD.SecurityId,
-//                    recD => (recD.Security?.Emitent.Name, recD.Security?.SalePrice))
-//                }).ToList();
-//            }
-//        }
+namespace SecuritiesDatabase.Implements
+{
+    public class RequestStorage : IRequestStorage
+    {
+        public List<RequestViewModel> GetFullList()
+        {
+            using (var context = new securitiesdbContext())
+            {
+                return context.Request
+                    .Include(rec => rec.Client)
+                    .Include(rec => rec.Agent)
+                    .Include(rec => rec.Bag)
+                    .ToList().Select(rec => new RequestViewModel
+                    {
+                        Id = rec.Id,
+                        RequestDate = (DateTime)rec.Date,
+                        RequestSum = (decimal)rec.Sum,
+                        ClientName = rec.Client.Fio,
+                        AgentName = rec.Agent.Fio,
+                        BagId = rec.Bag.Id
+                    }).ToList();
+            }
+        }
 
-//        public List<RequestViewModel> GetFilteredList(RequestBindingModel model)
-//        {
-//            if (model == null)
-//            {
-//                return null;
-//            }
-//            using (var context = new securitiesdbContext())
-//            {
-//                return context.Request
-//                    .Include(rec => rec.Bag)
-//                    .ThenInclude(rec => rec.Security)
-//                    .Where(rec => rec.Id.Equals(model.Id)).ToList()
-//                    .Select(rec => new RequestViewModel
-//                    {
-//                        Id = rec.Id,
-//                        RequestDate = (DateTime)rec.Date,
-//                        RequestSum = (decimal)rec.Sum,
-//                        ClientName = rec.Client.Fio,
-//                        AgentName = rec.Agent.Fio,
-//                        RequestSecurity = rec.Bag.ToDictionary(recD => recD.SecurityId, recD => (recD.Security?.Emitent.Name, recD.Security?.SalePrice))
-//                    }).ToList();
-//            }
-//        }
+        public List<RequestViewModel> GetFilteredList(RequestBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            using (var context = new securitiesdbContext())
+            {
+                return context.Request
+                    .Include(rec => rec.Client)
+                    .Include(rec => rec.Agent)
+                    .Include(rec => rec.Bag)
+                    .Where(rec => rec.Id.Equals(model.Id)).ToList()
+                    .Select(rec => new RequestViewModel
+                    {
+                        Id = rec.Id,
+                        RequestDate = (DateTime)rec.Date,
+                        RequestSum = (decimal)rec.Sum,
+                        ClientName = rec.Client.Fio,
+                        AgentName = rec.Agent.Fio,
+                        BagId = rec.Bag.Id
+                    }).ToList();
+            }
+        }
 
-//        public RequestViewModel GetElement(RequestBindingModel model)
-//        {
-//            if (model == null)
-//            {
-//                return null;
-//            }
+        public RequestViewModel GetElement(RequestBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
 
-//            using (var context = new securitiesdbContext())
-//            {
-//                var request = context.Request
-//                    .Include(rec => rec.Bag)
-//                    .ThenInclude(rec => rec.Security)
-//                    .FirstOrDefault(rec => rec.Id == model.Id);
-//                return request != null ?
-//                new RequestViewModel
-//                {
-//                    Id = request.Id,
-//                    RequestDate = (DateTime)request.Date,
-//                    RequestSum = (decimal)request.Sum,
-//                    ClientName = request.Client.Fio,
-//                    AgentName = request.Agent.Fio,
-//                    RequestSecurity = request.Bag.ToDictionary(recD => recD.SecurityId, recD => (recD.Security?.Emitent.Name, recD.Security?.SalePrice))
-//                } : null;
-//            }
-//        }
+            using (var context = new securitiesdbContext())
+            {
+                var request = context.Request
+                    .Include(rec => rec.Client)
+                    .Include(rec => rec.Agent)
+                    .Include(rec => rec.Bag)
+                    .FirstOrDefault(rec => rec.Id == model.Id);
+                return request != null ?
+                new RequestViewModel
+                {
+                    Id = request.Id,
+                    RequestDate = (DateTime)request.Date,
+                    RequestSum = (decimal)request.Sum,
+                    ClientName = request.Client.Fio,
+                    AgentName = request.Agent.Fio,
+                    BagId = request.Bag.Id
+                } : null;
+            }
+        }
 
-//        public void Insert(RequestBindingModel model)
-//        {
-//            using (var context = new securitiesdbContext())
-//            {
-//                using (var transaction = context.Database.BeginTransaction())
-//                {
-//                    try
-//                    {
-//                        CreateModel(model, new Request(), context);
-//                        context.SaveChanges();
-//                        transaction.Commit();
-//                    }
-//                    catch
-//                    {
-//                        transaction.Rollback();
-//                        throw;
-//                    }
-//                }
-//            }
-//        }
+        public void Insert(RequestBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        CreateModel(model, new Request(), context);
+                        context.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
 
-//        public void Update(RequestBindingModel model)
-//        {
-//            using (var context = new securitiesdbContext())
-//            {
-//                using (var transaction = context.Database.BeginTransaction())
-//                {
-//                    try
-//                    {
-//                        var element = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
+        public void Update(RequestBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var element = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
 
-//                        if (element == null)
-//                        {
-//                            throw new Exception("Элемент не найден");
-//                        }
+                        if (element == null)
+                        {
+                            throw new Exception("Элемент не найден");
+                        }
 
-//                        CreateModel(model, element, context);
-//                        context.SaveChanges();
+                        CreateModel(model, element, context);
+                        context.SaveChanges();
 
-//                        transaction.Commit();
-//                    }
-//                    catch
-//                    {
-//                        transaction.Rollback();
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
 
-//                        throw;
-//                    }
-//                }
-//            }
-//        }
+                        throw;
+                    }
+                }
+            }
+        }
 
-//        public void Delete(RequestBindingModel model)
-//        {
-//            using (var context = new securitiesdbContext())
-//            {
-//                Request element = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
-//                if (element != null)
-//                {
-//                    context.Request.Remove(element);
-//                    context.SaveChanges();
-//                }
-//                else
-//                {
-//                    throw new Exception("Элемент не найден");
-//                }
-//            }
-//        }
+        public void Delete(RequestBindingModel model)
+        {
+            using (var context = new securitiesdbContext())
+            {
+                Request element = context.Request.FirstOrDefault(rec => rec.Id == model.Id);
+                if (element != null)
+                {
+                    context.Request.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Элемент не найден");
+                }
+            }
+        }
 
-//        private Request CreateModel(RequestBindingModel model, Request request, securitiesdbContext context)
-//        {
-//            request.Date = model.RequestDate;
-//            request.Sum = model.RequestSum;
-//            request.ClientId = model.ClientId;
-//            request.AgentId = model.AgentId;
+        private Request CreateModel(RequestBindingModel model, Request request, securitiesdbContext context)
+        {
+            request.Date = model.RequestDate;
+            request.Sum = model.RequestSum;
+            request.ClientId = model.ClientId;
+            request.AgentId = model.AgentId;
+            request.BagId = model.BagId;
 
-//            if (request.Id == 0)
-//            {
-//                context.Request.Add(request);
-//                context.SaveChanges();
-//            }
-
-//            if (model.Id.HasValue)
-//            {
-//                var Bag = context.Bag
-//                    .Where(rec => rec.RequestId == model.Id.Value)
-//                    .ToList();
-
-//                context.Bag
-//                    .RemoveRange(Bag
-//                        .Where(rec => !model.RequestSecurity
-//                            .ContainsKey((int)rec.SecurityId))
-//                                .ToList());
-//                context.SaveChanges();
-
-//                foreach (var updateDetail in Bag)
-//                {
-//                    updateDetail.Sum = model.RequestSecurity[(int)updateDetail.SecurityId].Item2;
-//                    model.RequestSecurity.Remove((int)updateDetail.SecurityId);
-//                }
-
-//                context.SaveChanges();
-//            }
-
-//            foreach (var ed in model.RequestSecurity)
-//            {
-//                context.Bag.Add(new Bag
-//                {
-//                    RequestId = request.Id,
-//                    SecurityId = ed.Key,
-//                    Sum = ed.Value.Item2
-//                });
-
-//                context.SaveChanges();
-//            }
-
-//            return request;
-//        }
-//    }
-//}
+            return request;
+        }
+    }
+}
