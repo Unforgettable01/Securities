c68fefe2e5d3669aec891613121128df48f0218a
Unforgettable01 <boryanich01@gmail.com>
2022-01-13 18:47:23 +0400

    справочник клинты готов

--- Securities/FormClients.Designer.cs
+++ Securities/FormClients.Designer.cs
@@ -47,120 +47,138 @@ namespace Securities
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(1038, 152);
+            this.textBoxPassword.Location = new System.Drawing.Point(692, 99);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(288, 26);
+            this.textBoxPassword.Size = new System.Drawing.Size(193, 20);
             this.textBoxPassword.TabIndex = 26;
             // 
             // textBoxLogin
             // 
-            this.textBoxLogin.Location = new System.Drawing.Point(1038, 120);
+            this.textBoxLogin.Location = new System.Drawing.Point(692, 78);
+            this.textBoxLogin.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(288, 26);
+            this.textBoxLogin.Size = new System.Drawing.Size(193, 20);
             this.textBoxLogin.TabIndex = 25;
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(1038, 88);
+            this.textBoxName.Location = new System.Drawing.Point(692, 57);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(288, 26);
+            this.textBoxName.Size = new System.Drawing.Size(193, 20);
             this.textBoxName.TabIndex = 24;
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(965, 151);
+            this.labelPassword.Location = new System.Drawing.Point(643, 98);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(67, 20);
+            this.labelPassword.Size = new System.Drawing.Size(45, 13);
             this.labelPassword.TabIndex = 23;
             this.labelPassword.Text = "Пароль";
             // 
             // labelLogin
             // 
             this.labelLogin.AutoSize = true;
-            this.labelLogin.Location = new System.Drawing.Point(977, 120);
+            this.labelLogin.Location = new System.Drawing.Point(651, 78);
+            this.labelLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelLogin.Name = "labelLogin";
-            this.labelLogin.Size = new System.Drawing.Size(55, 20);
+            this.labelLogin.Size = new System.Drawing.Size(38, 13);
             this.labelLogin.TabIndex = 22;
             this.labelLogin.Text = "Логин";
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(985, 88);
+            this.labelName.Location = new System.Drawing.Point(657, 57);
+            this.labelName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(47, 20);
+            this.labelName.Size = new System.Drawing.Size(34, 13);
             this.labelName.TabIndex = 21;
             this.labelName.Text = "ФИО";
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(1176, 196);
+            this.buttonDelete.Location = new System.Drawing.Point(784, 127);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(118, 56);
+            this.buttonDelete.Size = new System.Drawing.Size(79, 36);
             this.buttonDelete.TabIndex = 20;
             this.buttonDelete.Text = "Удалить";
             this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // buttonRefresh
             // 
-            this.buttonRefresh.Location = new System.Drawing.Point(1176, 268);
+            this.buttonRefresh.Location = new System.Drawing.Point(784, 174);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(2);
             this.buttonRefresh.Name = "buttonRefresh";
-            this.buttonRefresh.Size = new System.Drawing.Size(118, 56);
+            this.buttonRefresh.Size = new System.Drawing.Size(79, 36);
             this.buttonRefresh.TabIndex = 19;
             this.buttonRefresh.Text = "Обновить";
             this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
             // 
             // buttonUpdate
             // 
-            this.buttonUpdate.Location = new System.Drawing.Point(1038, 268);
+            this.buttonUpdate.Location = new System.Drawing.Point(692, 174);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(2);
             this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(118, 56);
+            this.buttonUpdate.Size = new System.Drawing.Size(79, 36);
             this.buttonUpdate.TabIndex = 18;
             this.buttonUpdate.Text = "Изменить";
             this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(1038, 196);
+            this.buttonSave.Location = new System.Drawing.Point(692, 127);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(118, 56);
+            this.buttonSave.Size = new System.Drawing.Size(79, 36);
             this.buttonSave.TabIndex = 17;
             this.buttonSave.Text = "Сохранить";
             this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
             // dataGridViewAgents
             // 
             this.dataGridViewAgents.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridViewAgents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAgents.Location = new System.Drawing.Point(12, 14);
+            this.dataGridViewAgents.Location = new System.Drawing.Point(8, 9);
+            this.dataGridViewAgents.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridViewAgents.Name = "dataGridViewAgents";
             this.dataGridViewAgents.RowHeadersWidth = 62;
             this.dataGridViewAgents.RowTemplate.Height = 28;
-            this.dataGridViewAgents.Size = new System.Drawing.Size(880, 635);
+            this.dataGridViewAgents.Size = new System.Drawing.Size(587, 413);
             this.dataGridViewAgents.TabIndex = 16;
+            this.dataGridViewAgents.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridViewAgents_RowStateChanged);
             // 
             // textBoxPassportNumber
             // 
-            this.textBoxPassportNumber.Location = new System.Drawing.Point(1038, 56);
+            this.textBoxPassportNumber.Location = new System.Drawing.Point(692, 36);
+            this.textBoxPassportNumber.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxPassportNumber.Name = "textBoxPassportNumber";
-            this.textBoxPassportNumber.Size = new System.Drawing.Size(288, 26);
+            this.textBoxPassportNumber.Size = new System.Drawing.Size(193, 20);
             this.textBoxPassportNumber.TabIndex = 28;
             // 
             // labelPassportNumber
             // 
             this.labelPassportNumber.AutoSize = true;
-            this.labelPassportNumber.Location = new System.Drawing.Point(898, 56);
+            this.labelPassportNumber.Location = new System.Drawing.Point(599, 36);
+            this.labelPassportNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPassportNumber.Name = "labelPassportNumber";
-            this.labelPassportNumber.Size = new System.Drawing.Size(134, 20);
+            this.labelPassportNumber.Size = new System.Drawing.Size(91, 13);
             this.labelPassportNumber.TabIndex = 27;
             this.labelPassportNumber.Text = "Номер паспорта";
             // 
             // FormClients
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1344, 661);
+            this.ClientSize = new System.Drawing.Size(896, 430);
             this.Controls.Add(this.textBoxPassportNumber);
             this.Controls.Add(this.labelPassportNumber);
             this.Controls.Add(this.textBoxPassword);
@@ -174,6 +192,7 @@ namespace Securities
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.dataGridViewAgents);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormClients";
             this.Text = "Клиенты";
             this.Load += new System.EventHandler(this.FormClients_Load);
