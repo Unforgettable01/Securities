a41f6c1707abdb91bee0e8237b5609d2ea367896
Unforgettable01 <boryanich01@gmail.com>
2022-01-13 17:39:21 +0400

    справочник - эмитент полностью готов

--- Securities/FormEmitents.Designer.cs
+++ Securities/FormEmitents.Designer.cs
@@ -42,24 +42,27 @@ namespace Securities
             // labelName
             // 
             this.labelName.AutoSize = true;
-            this.labelName.Location = new System.Drawing.Point(1137, 299);
+            this.labelName.Location = new System.Drawing.Point(758, 194);
+            this.labelName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(122, 20);
+            this.labelName.Size = new System.Drawing.Size(83, 13);
             this.labelName.TabIndex = 0;
             this.labelName.Text = "Наименование";
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(1047, 331);
+            this.textBoxName.Location = new System.Drawing.Point(698, 215);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(294, 26);
+            this.textBoxName.Size = new System.Drawing.Size(197, 20);
             this.textBoxName.TabIndex = 1;
             // 
             // buttonSave
             // 
-            this.buttonSave.Location = new System.Drawing.Point(1141, 363);
+            this.buttonSave.Location = new System.Drawing.Point(761, 236);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(118, 56);
+            this.buttonSave.Size = new System.Drawing.Size(79, 36);
             this.buttonSave.TabIndex = 2;
             this.buttonSave.Text = "Сохранить";
             this.buttonSave.UseVisualStyleBackColor = true;
@@ -67,47 +70,55 @@ namespace Securities
             // 
             // buttonUpdate
             // 
-            this.buttonUpdate.Location = new System.Drawing.Point(1141, 425);
+            this.buttonUpdate.Location = new System.Drawing.Point(761, 276);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.Size = new System.Drawing.Size(118, 56);
+            this.buttonUpdate.Size = new System.Drawing.Size(79, 36);
             this.buttonUpdate.TabIndex = 3;
             this.buttonUpdate.Text = "Изменить";
             this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
             // buttonRefresh
             // 
-            this.buttonRefresh.Location = new System.Drawing.Point(1141, 487);
+            this.buttonRefresh.Location = new System.Drawing.Point(761, 317);
+            this.buttonRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonRefresh.Name = "buttonRefresh";
-            this.buttonRefresh.Size = new System.Drawing.Size(118, 56);
+            this.buttonRefresh.Size = new System.Drawing.Size(79, 36);
             this.buttonRefresh.TabIndex = 4;
             this.buttonRefresh.Text = "Обновить";
             this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
             // 
             // buttonDelete
             // 
-            this.buttonDelete.Location = new System.Drawing.Point(1141, 549);
+            this.buttonDelete.Location = new System.Drawing.Point(761, 357);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(118, 56);
+            this.buttonDelete.Size = new System.Drawing.Size(79, 36);
             this.buttonDelete.TabIndex = 5;
             this.buttonDelete.Text = "Удалить";
             this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // dataGridViewEmitents
             // 
             this.dataGridViewEmitents.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridViewEmitents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewEmitents.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewEmitents.Location = new System.Drawing.Point(8, 8);
+            this.dataGridViewEmitents.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridViewEmitents.Name = "dataGridViewEmitents";
             this.dataGridViewEmitents.RowHeadersWidth = 62;
             this.dataGridViewEmitents.RowTemplate.Height = 28;
-            this.dataGridViewEmitents.Size = new System.Drawing.Size(995, 593);
+            this.dataGridViewEmitents.Size = new System.Drawing.Size(663, 385);
             this.dataGridViewEmitents.TabIndex = 6;
+            this.dataGridViewEmitents.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridViewEmitents_RowStateChanged);
             // 
             // FormEmitents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1353, 628);
+            this.ClientSize = new System.Drawing.Size(902, 408);
             this.Controls.Add(this.dataGridViewEmitents);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonRefresh);
@@ -115,6 +126,7 @@ namespace Securities
             this.Controls.Add(this.buttonSave);
             this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.labelName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormEmitents";
             this.Text = "Эмитенты";
             this.Load += new System.EventHandler(this.FormEmitents_Load);
